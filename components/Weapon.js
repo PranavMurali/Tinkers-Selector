a2a388244ff155a13557eda5e1735a39b0078562
Pranav Murali <pranavmurali2001@gmail.com>
2022-01-03 23:56:03 +0530

    :date: Data updates and site logic revamp
    - Reset data
    - Revamp site logic and redo major working logic
    - Change weapon display for better calculations

--- components/Weapon.js
+++ components/Weapon.js
@@ -40,16 +40,48 @@ const Weapon = ({query}) => {
         <div className="bg-purple-600 rounded shadow border border-black p-6 w-64">
             <h5 className="text-3xl font-bold mb-4 mt-0" key="{results.Material}">{results.Tool}</h5>
         <div>
-            <p className="text-gray-300 text-base">Head</p>
-            <p className="text-yellow-300 text-sm"> {results.Head}</p>
+            <p className="text-gray-300 text-base">Attack Speed</p>
+            <p className="text-yellow-300 text-sm"> {results.AttSpd}</p>
         </div>
         <div>
-        <p className="text-gray-300 text-base">Handle</p>
-            <p className="text-yellow-300 text-sm"> {results.Handle}</p>
+            <p className="text-gray-300 text-base">Knockback</p>
+            <p className="text-yellow-300 text-sm"> {results.Knockback}</p>
         </div>
         <div>
-        <p className="text-gray-300 text-base">Extra</p>
-            <p className="text-yellow-300 text-sm"> {results.Extras}</p>
+            <p className="text-gray-300 text-base">Mining Speed</p>
+            <p className="text-yellow-300 text-sm"> {results.MinSpdMod}</p>
+        </div>
+        <div>
+            <p className="text-gray-300 text-base">Damage Multiplier</p>
+            <p className="text-yellow-300 text-sm"> {results.DmgMult}</p>
+        </div>
+        <div>
+            <p className="text-gray-300 text-base">Damage Modifier</p>
+            <p className="text-yellow-300 text-sm"> {results.DmgMod}</p>
+        </div>
+        <div>
+            <p className="text-gray-300 text-base">Durability Modifier</p>
+            <p className="text-yellow-300 text-sm"> {results.DuraMod}</p>
+        </div>
+        <div>
+            <p className="text-gray-300 text-base">Bonus Damage</p>
+            <p className="text-yellow-300 text-sm"> {results.BonusDmg}</p>
+        </div>
+        <div>
+            <p className="text-gray-300 text-base">Damage Cutoff</p>
+            <p className="text-yellow-300 text-sm"> {results.DmgCutoff}</p>
+        </div>
+        <div>
+            <p className="text-gray-300 text-base">Part 1</p>
+            <p className="text-yellow-300 text-sm"> {results.c1}</p>
+        </div>
+        <div>
+            <p className="text-gray-300 text-base">Part 2</p>
+            <p className="text-yellow-300 text-sm"> {results.c2}</p>
+        </div>
+        <div>
+            <p className="text-gray-300 text-base">Part 3</p>
+            <p className="text-yellow-300 text-sm"> {results.c3}</p>
         </div>
         <button className="bg-green-700 rounded shadow border border-black h-10 w-10 text-center self-end" onClick={(e) => {click(e, results.Tool,results.Head,results.Handle,results.Extras);}}>add</button>
 
